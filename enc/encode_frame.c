f357938d7e2afb62c750ddad29d61d554bc41de9
Guillaume Martres <smarter@ubuntu.com>
2015-11-08 15:53:06 +0100

    no filter

--- enc/encode_frame.c
+++ enc/encode_frame.c
@@ -94,6 +94,7 @@ static int dering_decision(int k, int l, yuv_frame_t *rec, yuv_frame_t *org, con
   int filtered = (filtered_error + (int32_t)(lambda*filtered_rate + 0.5)) <
                  (unfiltered_error + (int32_t)(lambda*unfiltered_rate + 0.5));
 
+  filtered = 0;
   putbits(1, filtered, (stream_t*)stream);
   return filtered;
 }
